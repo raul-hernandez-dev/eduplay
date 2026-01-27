c06ed830362345e271dbc3aa6a4037e7496f6f20
raulhdz_dev <raul.hernandez.lopez.dev@gmail.com>
2026-01-27 17:51:25 -0600

    Language context configuration

---
+++ src/contexts/LanguageContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const LanguageContext = createContext({});
+
+export const useLanguage = () => useContext(LanguageContext);
+
+export const LanguageProvider = ({ children }) => {
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    setLanguage(lang);
+    localStorage.setItem('language', lang);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
